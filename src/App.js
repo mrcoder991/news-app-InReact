c6da93c17c5ef7a9fc766bcc4ec26a63a3f8bbce
mrcoder991 <udaygirhepunje41@gmail.com>
2022-03-12 21:03:08 +0530

    created required components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
-      <div>This is react class based component</div>
+      <div>
+        <News/>
+      </div>
     )
   }
 }
