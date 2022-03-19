dfe36028b6eabee9f011b463ec02f0be33550c3b
mrcoder991 <udaygirhepunje41@gmail.com>
2022-03-19 12:00:55 +0530

    added infinite scrolling

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
 
 export class News extends Component {
   static defaultProps = {
@@ -19,9 +22,10 @@ export class News extends Component {
   constructor(props){
     super(props);
     this.state = {
-        articles: [],
-        loading: false,
-        page:1
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0
     }
     document.title = `${this.capitalize(this.props.category)} | News monkey`;
     }
@@ -35,54 +39,53 @@ export class News extends Component {
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false
-  })
-    }
+    })
+  }
   
     async componentDidMount(){ 
       this.updateNews();
-    }
-
-    handlePrevClick = async ()=>{
-      this.setState({
-        page : this.state.page -1
-      })
-      this.updateNews();
-
-    }
-
-    handleNextClick = async ()=>{
-      // console.log("Next"); 
-      if (this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
-      }
-      else{
-       this.setState({
-        page : this.state.page + 1
-       })
-        this.updateNews();
-      }
-    }
+  }
   
+   fetchMoreData = async () =>  {
+    this.setState({page: this.state.page + 1})
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({
+        articles: this.state.articles.concat(parsedData.articles),
+        totalResults: parsedData.totalResults
+    })
+  };
+
     capitalize = (word)=>{
       const lower = word.toLowerCase();
       return lower.charAt(0).toUpperCase() + lower.slice(1);
-    }
+  }
+  
 
   render() {
     return (
       <div className="container my-4">
-        <h1 className="text-light text-center">News monkey - Top headlines from {this.capitalize(this.props.category)} Category</h1>
-        {this.state.loading && <Spinner />}
-        <div className="row my-4">
-          {!this.state.loading && this.state.articles.map((element)=>{
-          return <div key={element.url} className="col-md-4">
-            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage ? element.urlToImage : "https://www.caspianpolicy.org/no-image.png"} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
-          </div>
-          })}
-          <div className='container d-flex justify-content-between my-4'>
-            <button type="button" disabled={this.state.page<=1} className="btn btn-light m-2" onClick={this.handlePrevClick}>&larr; Previous</button>
-            <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-light m-2" onClick={this.handleNextClick}>Next &rarr;</button>
-          </div>
-        </div>
+        <h1 className="text-light text-center">News monkey - Top {this.capitalize(this.props.category)} headlines</h1>
+
+          <InfiniteScroll
+            dataLength={this.state.articles.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.articles.length !== this.state.totalResults}
+            loader={<Spinner/>}
+          >
+            <div className="container">
+              <div className="row my-4">
+                {this.state.articles.map((element)=>{
+                return <div key={element.url} className="col-md-4">
+                  <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage ? element.urlToImage : "https://www.caspianpolicy.org/no-image.png"} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
+                </div>
+                })}
+              </div>
+            </div>
+          </InfiniteScroll>
+
+          
       </div>
     )
   }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ import Loading from './loading.svg'
 export default class Spinner extends Component {
   render() {
     return (
-        <div className="d-flex align-items-center justify-content-center"  style={{minHeight: '100vh' }}>
+        <div className="d-flex align-items-center justify-content-center"  style={{minHeight: '30vh' }}>
         <img src={Loading} alt="Loading" style={{width: '5em'}}/>
       </div>
     )
